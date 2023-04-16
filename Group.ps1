5f907bb3185848da6bebf1ca0a4077763f23f935
DvirEzra <122629905+DvirEzra@users.noreply.github.com>
2023-04-16 21:35:02 +0300

    Add files via upload

---
+++ Group.ps1
@@ -0,0 +1,15 @@
+﻿# Specify here the username whose password to reset.
+$file = Get-Content -Path c:\temp\pass.txt
+# Import the System.Web .NET assembly
+foreach ($username in $file)
+{
+Add-Type -AssemblyName 'System.Web'
+# Generate a random password that is 12-characters long with five non-AlphaNumeric characters.
+$randomPassword = [System.Web.Security.Membership]::GeneratePassword(20, 5)
+# Convert the plain text password to a secure string.
+$newPassword = $randomPassword | ConvertTo-SecureString -AsPlainText -Force
+# Reset the user password
+Set-ADAccountPassword -Identity $username -NewPassword $newPassword -Reset
+# Display the new password
+$randomPassword
+}
